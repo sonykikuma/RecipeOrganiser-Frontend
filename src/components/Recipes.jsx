ec6a43f8d785fd031ba7617ff082eb9593a29704
sonykikuma <er.abhasoni@gmail.com>
2024-11-16 17:54:35 +0530

    fix: card body data

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -89,15 +89,55 @@ const Recipes = () => {
                 <h5 className="card-title">{recipe.title}</h5>
                 <p>
                   <strong>Cuisine Type: </strong>
-                  <Link to={`/recipes/${recipe._id}`}> See Recipe </Link>
+                  {recipe.recipeType}
                 </p>
                 <p>
                   <strong>Ingredients: </strong>
-                  <Link to={`/recipes/${recipe._id}`}> See Recipe </Link>
+                  <Link
+                    to={`/recipes/${recipe._id}`}
+                    style={{
+                      textDecoration: "underline",
+                    }}
+                  >
+                    {" "}
+                    See Recipe{" "}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      class="bi bi-chevron-right"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"
+                      />
+                    </svg>{" "}
+                  </Link>
                 </p>
                 <p>
                   <strong>Instructions: </strong>
-                  {recipe.recipeType}
+                  <Link
+                    to={`/recipes/${recipe._id}`}
+                    style={{ textDecoration: "underline" }}
+                  >
+                    {" "}
+                    See Recipe{" "}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      className="bi bi-chevron-right "
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"
+                      />
+                    </svg>{" "}
+                  </Link>
                 </p>
 
                 <button
